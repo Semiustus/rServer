710919fd9235dde9e1364165b34a0ed94add347c
Semiustus <wartime@outlook.com>
2015-05-28 00:56:51 -0700

    FIles

---
+++ testing_ignore/listComPorts.js
@@ -0,0 +1,32 @@
+// JavaScript source code
+var serialport = require("serialport");
+var SerialPort = serialport.SerialPort;
+
+// list serial ports:
+serialport.list(function (err, ports) {
+    ports.forEach(function (port) {
+        console.log(port.comName);
+    });
+});
+
+var portName = "COM6"
+var receivedData_A = "";
+var serialPortA = new SerialPort(portName, {
+    baudrate: 115200,
+    // defaults for Arduino serial communication
+    dataBits: 8,
+    buffersize: 1000,
+    parity: 'none',
+    stopBits: 1,
+    flowControl: false
+});
+
+serialPortA.on("open", function () {
+    console.log('open');
+
+    serialPortA.on('data', function (data) {
+        console.log('data received: ' + data);
+        receivedData_A = data;
+        //console.log('checking variable: ' + receivedData_A);
+        });
+});
